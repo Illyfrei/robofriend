0b6e8f2e58def8adab3ace7dfd6f636df7aa0150
illy freidin <illyfreishap@gmail.com>
2020-05-12 15:39:41 +0200

    Updates

---
+++ precache-manifest.8cdf92ce0047405106d4f23508314946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404474a2c21d1f4aea4c5d422d7bd5e7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cdc44b55ed1b150deca0",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "28cd3a6549e49d30cae5",
+    "url": "/robofriends/static/css/main.ddd0b546.chunk.css"
+  },
+  {
+    "revision": "cdc44b55ed1b150deca0",
+    "url": "/robofriends/static/js/2.a73e7871.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a73e7871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28cd3a6549e49d30cae5",
+    "url": "/robofriends/static/js/main.b60a0d60.chunk.js"
+  },
+  {
+    "revision": "5048e69972ec4d5ebeb1",
+    "url": "/robofriends/static/js/runtime-main.9e81d2bc.js"
+  }
+]);
